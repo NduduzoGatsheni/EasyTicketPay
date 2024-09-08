daea7a0f426cf9f536a8fb67b66389ac108080bd
Nduduzo635 <nduvane635@gmail.com>
2024-09-08 23:01:28 +0200

    card changes

--- src/app/view-card/view-card.page.ts
+++ src/app/view-card/view-card.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-view-card',
   templateUrl: './view-card.page.html',
@@ -11,5 +11,10 @@ export class ViewCardPage implements OnInit {
 
   ngOnInit() {
   }
-
+  @Input() name: string = 'NDUDUZO NDLOVU';
+  @Input() title: string = 'EasyPayTicket';
+  @Input() email: string = 'nduduzondlovu635@gmail.com';
+  @Input() phone: string = '074 0998713';
+  @Input() address: string = '24 mut road';
+  @Input() qrCodeData: string = 'qr code data...';
 }
